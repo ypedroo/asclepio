acb87abaaa28d898d3b36272283e32146249b74b
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-05-03 09:58:19 -0300

    create model

---
+++ src/asclepio/asclepio/Models/Patient.cs
@@ -0,0 +1,11 @@
+﻿namespace asclepio.Models
+{
+    public class Patient
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsDizzy { get; set; }
+        public string EmergencyContact { get; set; }
+        public string DoctorName { get; set; }
+    }
+}
