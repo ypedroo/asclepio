ab74e9ebab4986b030d04ed8e9704ef02ed3e640
Ynoa Pedro <ynoa.pedro@outlook.com>
2020-05-03 12:03:28 -0300

    Change POC to api

---
+++ src/asclepio/asclepio-api/Services/DecisionService.cs
@@ -0,0 +1,25 @@
+﻿using asclepio.infra.DecisionComponent.Models;
+using asclepio_infra.DecisionComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asclepio_api.Services
+{
+    public class DecisionService
+    {
+        private readonly IDecision _decision;
+
+        public DecisionService(IDecision decision)
+        {
+            _decision = decision;
+        }
+
+        public Patient AnalyzePatient(Patient patient)
+        {
+            var analyzedPatient = _decision.Analyze(patient);
+            return analyzedPatient;
+        }
+    }
+}
